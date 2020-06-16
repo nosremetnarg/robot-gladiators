ad770ba789432526eca60207d2a161c56fce37b8
nosremetnarg <grant.alex.emerson@gmail.com>
2020-06-16 06:39:14 -0500

    can now fight all Robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,15 +3,19 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
+// Game States
+// "WIN" - Player robot has defeated all enemy robots
+// * Fight all enemy robots
+// * Defeat each enemy robot
+// "LOSE" - Player robot's health is zero or less
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
 
+var fight = function(enemyName) {
+ //fight function statements
 // Alert users that they are starting the round
 window.alert("Welcome to Robot Gladiators!");
 var promptFight = window.prompt("Would you like to FIGHT or Skip this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -81,4 +85,6 @@ if (playerHealth <= 0) {
     window.alert("You need to pick a valid option. Try again!");
 }
 };
-fight ();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
